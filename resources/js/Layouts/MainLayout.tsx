74e0a4f30faf9ccaed9eaf630922eb7a60eced59
Valerius Mattfeld <mail@valerius.me>
2024-08-07 19:39:05 +0200

    new frontend base layout

---
+++ resources/js/Layouts/MainLayout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+export default function MainLayout({ children }: PropsWithChildren) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
---
+++ resources/js/Pages/Contracts.tsx
@@ -0,0 +1,3 @@
+export default function Contracts() {
+    return <h1>Hallo</h1>
+}
